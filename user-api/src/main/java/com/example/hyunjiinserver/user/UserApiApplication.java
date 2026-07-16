0c282eb75a86fdfebc4941aedf7aaeb8dcccca3f
taek <wontaek88@gmail.com>
2026-07-17 02:05:08 +0900

    Configure JPA scanning for core module

--- user-api/src/main/java/com/example/hyunjiinserver/user/UserApiApplication.java
+++ user-api/src/main/java/com/example/hyunjiinserver/user/UserApiApplication.java
@@ -2,8 +2,12 @@ package com.example.hyunjiinserver.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.example.hyunjiinserver")
+@EntityScan(basePackages = "com.example.hyunjiinserver.core")
+@EnableJpaRepositories(basePackages = "com.example.hyunjiinserver.core")
 public class UserApiApplication {
 
     public static void main(String[] args) {
